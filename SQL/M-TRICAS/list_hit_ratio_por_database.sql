40ff5c48f4c6c998212513aa65b41befa1eb4228
GUGA <gustavo@scsilva.com>
2018-11-23 17:54:12 -0200

    Update and rename list_hit_ratio_por_database.sql to lista_hit_ratio_por_database.sql

--- SQL/M-TRICAS/list_hit_ratio_por_database.sql
+++
@@ -1,7 +0,0 @@
-SELECT d.datname,
-        round((blks_hit::float/(blks_read+blks_hit+1)*100)::numeric, 2) as cachehitratio,
-        FROM pg_stat_database sd
-        JOIN pg_database d ON d.oid = sd.datid
-        WHERE d.datallowconn AND NOT d.datistemplate
-        AND (blks_read+blks_hit) > 0
-        ORDER BY datname, cachehitratio;
